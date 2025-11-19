8be835c8bea6b980bff67b012aed9217d31a2f6c
fengjutian <fjutian@foxmail.com>
2025-11-19 21:52:46 +0800

    feat(商家列表): 添加搜索框和商家列表项组件
    - 新增搜索框和搜索按钮的布局
    - 添加商家列表项组件，包含头像和安装按钮
    - 调整页面布局样式

--- src/pages/merchant_list/index.tsx
+++ src/pages/merchant_list/index.tsx
@@ -1,36 +1,72 @@
 import React from 'react';
-import { Layout, Text, Button, Input} from '@ui-kitten/components';
+import {View, ImageProps} from 'react-native';
+import { Layout, Text, Button, Input, ButtonGroup, Avatar, ListItem} from '@ui-kitten/components';
 import { StyleSheet } from 'react-native';
+
+const InstallButton = () => (
+  <Button size='tiny'>
+    INSTALL
+  </Button>
+);
+
 export default function MerchantList(): React.ReactElement {
 
    const [value, setValue] = React.useState('');
+
+   const ItemImage = (props: ImageProps): React.ReactElement => (
+      <Avatar
+        {...props}
+        style={[props.style, styles.itemImage]}
+
+      />
+    );
+
+
   return (
     <Layout style={styles.container}>
-       <Input
-        placeholder='请输入'
-        value={value}
-        onChangeText={nextValue => setValue(nextValue)}
+      <View style={styles.searchContainer} >
+        <Input
+          placeholder='请输入'
+          value={value}
+          style={{width: '80%', marginRight: 8}}
+          onChangeText={nextValue => setValue(nextValue)}
+        />
+        <Button
+          appearance='outline'
+          status='primary'
+        >搜索</Button>
+      </View>
+
+      <ListItem
+        title='UI Kitten'
+        description='A set of React Native components'
+        accessoryLeft={ItemImage}
+        accessoryRight={InstallButton}
       />
-      <Button
-        style={styles.button}
-        appearance='outline'
-        status='primary'
-      >
-        搜索
-      </Button>
+
+
+
     </Layout>
   );
 }
 
 const styles = StyleSheet.create({
+   itemImage: {
+    tintColor: null,
+  },
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
     padding: 16,
     width: '100%'
   },
   button: {
     marginTop: 16
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    // alignItems: 'center',
+    justifyContent: 'flex-start',
   }
 });
